0b4411afb3e68f51b958839407bc1203b6e1f568
KWAshen <ashen.maliaha@gmail.com>
2025-07-14 21:39:15 +0200

    salvataggio del terzo esercizio

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function iniziali(array, letter){
+  const filter = [];
 
+  for(let i=0; i<Array.length; i++){
+
+    if(array[i][0].minuscolo() === letter.minuscolo()){
+      filter.push(array[i]);
+    }
+  }
+  return filter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names, 'a'))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
