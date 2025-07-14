924a6c33bcf5e1d5f63fda0161e33e7d1e51f0f5
KWAshen <ashen.maliaha@gmail.com>
2025-07-14 21:28:15 +0200

    secondo esercizio

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function iniziali(array){
+  const iniziali = [];
+  
+  for(let i=0; i<Array.length; i++){
+    
+    iniziali.push(array[i][0]);
+  }
+  return iniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
