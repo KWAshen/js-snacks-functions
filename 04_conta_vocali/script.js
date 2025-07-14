151385fb7c584c54f40f3baced34e231d4f0f268
KWAshen <ashen.maliaha@gmail.com>
2025-07-14 21:48:27 +0200

    salvataggio quarto esercizio

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -3,11 +3,24 @@
 const word = 'javascript';
 
 
-// Dichiara la funzione qui.
+// Dichiara la funzione qui.ù
+function contavocali(stringa){
+
+  const vocali = ['a','e','i','0','u'];
+  let count = 0;
+
+  for(let i=0; i<stringa.length; i++){
+    if(vocali.includes(stringa[i])){
+      count++;
+    }
+  }
+  return count
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+count.log(contavocali(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
